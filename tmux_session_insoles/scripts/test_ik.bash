0c98c51782ba797204d746cb7b38ceaa144fa14c
frederico-klein <frekle@kth.se>
2023-09-28 09:40:36 +0200

    needs cleanup!
    Former-commit-id: f40f1d60ff2b01cfb4929d5bc0bb2e9c1024603f

--- tmux_session_insoles/scripts/test_ik.bash
+++ tmux_session_insoles/scripts/test_ik.bash
@@ -2,7 +2,7 @@
 SESSION_NAME=insole
 
 source "`rospack find tmux_session_core`/common_functions.bash"
-ros_core_tmux "$SESSION_NAME"
+ros_core_tmux_clock "$SESSION_NAME"
 
 tmux set -g pane-border-status top
 
@@ -20,34 +20,22 @@ SUBJECT_NUM=2
 ACTION=walking
 WRENCH_DELAY=0.1
 INSOLE_START_TIME=1668001002.000000000 
-IK_START_SECS=
-IK_START_NSECS=
-CLOCK_START_SECS=
-CLOCK_START_NSECS=
+IK_START_SECS=1677844584
+IK_START_NSECS=722696000
+CLOCK_START_SECS=1677844580
+CLOCK_START_NSECS=0
 ## it seems like we are messing up with the publish_transforms=false, but we are actually republishing the values using the combined message with everything from the insoles, so it is okay
 ## another caveat is that the insoles delay is being completely overlooked by the republisher node which has its own delay for publishing onld COP transforms, so we set it to zero. 
 ## looking at the transforms for the wrench in rviz will also be wrong because we publish transformations that are backstamped, so they will show it in a past robot that doesnt exist anymore.
 
 #"roslaunch moticon_insoles play_ik.launch filename:=$IK_DATA_FILE --wait" 
 W1=(
-"roslaunch osrt_ros ${ID_NODE_LAUNCH} get_second_label:=false left_foot_tf_name:=left_filtered right_foot_tf_name:=right_filtered model_file:=$MODEL_FILE --wait" 
-"roslaunch moticon_insoles feet_wrench_and_ik_from_file.launch filename:=$INSOLE_DATA_FILE publish_transforms:=false output_left:=/grf_left/unfiltered output_right:=/grf_right/unfiltered estimated_delay:=0.0 foot_length:=0.2486 foot_width:=0.0902 grf_origin_z_offset:=$GRF_ORIGIN_Z_OFFSET start_time:=$INSOLE_START_TIME --wait" 
-"roslaunch moticon_insoles play_ik_2392.launch filename:=$IK_DATA_FILE start_at_secs:=$IK_START_SECS_START start_at_nsecs:=$IK_START_NSECS --wait" 
-"roslaunch republisher republisher_sync_insoles.launch wrench_delay:=$WRENCH_DELAY debug_publish_zero_cop:=false debug_publish_fixed_force:=false --wait" 
-"roslaunch osrt_ros so_round_robin_filtered_multi.launch n_proc:=4 model_file:=$MODEL_FILE moment_arm_library_path:=$MOMENT_ARM_LIB"
+"roslaunch moticon_insoles play_ik_2392.launch model_file:=$MODEL_FILE filename:=$IK_DATA_FILE start_at_secs:=$IK_START_SECS start_at_nsecs:=$IK_START_NSECS --wait" 
 "roslaunch custom_clock back_in_time_clock.launch start_at_secs:=$CLOCK_START_SECS start_at_nsecs:=$CLOCK_START_NSECS" 
-"rostopic echo /clock" 
 )
 W2=(
 "rosservice call /ik/outlabels --wait" 
-"sleep 3; rosservice call /moticon_insoles/start_playback --wait ; rosrun tf tf_monitor map calcn_r" 
-"sleep 3; rosservice call /inverse_kinematics_from_file/start --wait " 
-"rqt_graph " 
-"sleep 2; rosservice call /id_node/start_recording --wait" 
-"sleep 2; rosservice call /so_visualization/start_recording --wait" 
-"sleep 2; rosservice call /id_node/set_name_and_path \"{name: 's${SUBJECT_NUM}_id_${ACTION}_filtered_SCRIPT${ACTION_NUM}_', path: '/tmp/${SUBJECT_NUM}' }\" --wait" 
-"sleep 2; rosservice call /so_visualization/set_name_and_path \"{name: 's${SUBJECT_NUM}_id_${ACTION}_filtered_SCRIPT${ACTION_NUM}_', path: '/tmp/${SUBJECT_NUM}' }\" --wait" 
-"roslaunch osrt_ros vis_so_rr_multi.launch model_file:=$MODEL_FILE "
+"sleep 3; rosservice call /inverse_kinematics_from_file/start_at --wait " 
 )
 W3=(
 )
