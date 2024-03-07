d5305ab93c5f748cd5bbd5952cbe27d0e227daa4
frederico-klein <frekle@kth.se>
2024-03-07 17:30:05 +0100

    ar experiment
    Former-commit-id: c82b65f16b7ea98b18ecbb19551232118fd5d750

--- tmux_session_experiments/scripts/ar_imu_upperarm_acer_acquire.bash
+++ tmux_session_experiments/scripts/ar_imu_upperarm_acer_acquire.bash
@@ -4,6 +4,8 @@ SESSION_NAME=ar_imu_combined
 source "`rospack find tmux_session_core`/common_functions.bash"
 ros_core_tmux "$SESSION_NAME"
 
+tmux set -g pane-border-status top
+
 W1=(
 
 "rviz -d /catkin_ws/_cam_acer_tf.rviz" 
@@ -14,8 +16,13 @@ W1=(
 "ROS_NAMESPACE=usb_cam_acer rosrun image_proc image_proc" 
 )
 
+W2=(
+	"rosrun tmux_session_core session_commander_ar_imu.py"
+"#rosservice call /ar/ik_upperbody_node/set_name_and_path '' ''" 
+"#rosservice call /imu/ik_upperbody_node/set_name_and_path '' ''"
+)
 create_tmux_window "$SESSION_NAME" "rviz_urdf" "${W1[@]}"
-#create_tmux_window "$SESSION_NAME" "imu_calibs" "${W2[@]}"
+create_tmux_window "$SESSION_NAME" "session" "${W2[@]}"
 #create_tmux_window "$SESSION_NAME" "ik_imus" 	"${W3[@]}"
 
 tmux -2 a -t $SESSION_NAME
