0f8ab464b10f4f8db8f884d62f74c7e8148a9f9e
frekle <frekle@kth.se>
2024-05-22 15:04:17 +0000

    i am trying to fit another one 4

--- tmux_session_insoles/scripts/AUTOMATE.py
+++ tmux_session_insoles/scripts/AUTOMATE.py
@@ -4,7 +4,7 @@
 USE_TIMEOUT=False
 
 #trials_to_run = [0,1,2,3,4,5,6,7,8]
-trials_to_run = [4]
+trials_to_run = [5]
 
 #clock_slowdown_rate= 1
 clock_slowdown_rate=1
@@ -93,7 +93,7 @@ insole_start = [
  [1.0, 0.5  , 0.25 ],  #
  [1.0, 0.5  , 0.3  ],  #
  [1.3, 0.7  , 0.25  ], #
- [1.0, 0.5  , 0.4   ],
+ [1.0, 0.5  , 0.4   ], #
  [1.0, 0.1  , 0.45  ],
  [1.0, -0.5 , 0.3   ],
  [1.0, 0.0  , 0.0   ],
