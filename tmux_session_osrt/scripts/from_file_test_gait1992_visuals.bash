bb34a64580d8bc2f23614129967ffe8dc4953210
frekle <frekle@kth.se>
2024-05-22 07:54:56 +0000

    updated to new namespacing definitions

--- tmux_session_osrt/scripts/from_file_test_gait1992_visuals.bash
+++ tmux_session_osrt/scripts/from_file_test_gait1992_visuals.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+FILE=/srv/host_data/tmp/04/test_walking_186/2024-05-13-14-17-46s04_id_walking_filtered_SCRIPT0_1ik.sto
+
 tmux new-session -s mysession -d 
 tmux set-option -s -t mysession default-command "bash --rcfile ~/.bashrc_ws.sh"
 tmux send -t mysession:0.0 "roscore" C-m
@@ -23,17 +25,18 @@ tmux split-window -v -p 50
 #tmux select-pane -t 0
 
 #sends keys to first and second terminals
-tmux send -t mysession:1.0 "sleep 2; rosservice call /ik/outlabels" C-m
+tmux send -t mysession:1.0 "sleep 2; rosservice call /ik/out_labels" C-m
 tmux send -t mysession:1.1 "rostopic echo /model_generic/joint_states" C-m
 tmux send -t mysession:1.2 "roslaunch osrt_ros vis_ik.launch" C-m
 
-tmux send -t mysession:1.3 "roslaunch gait1992_description human_control.launch" C-m
-tmux send -t mysession:1.4 "roslaunch osrt_ros ik_bare_1992.launch" C-m
+tmux send -t mysession:1.3 "roslaunch gait1992_description human_control.launch tf_prefix:=/ik" C-m
+
+tmux send -t mysession:1.4 "roslaunch osrt_ros ik_bare_1992.launch ik_file:=${FILE}" C-m
 ## this can also play ik with size 23 for a very similar lowerbody model. scaling is not done, so distances will be off.
 #tmux send -t mysession:1.4 "roslaunch osrt_ros ik_bare_2392.launch" C-m
 tmux send -t mysession:1.5 "rosrun rqt_graph rqt_graph" C-m
 tmux send -t mysession:1.6 "roslaunch osrt_ros custom.launch" C-m
-tmux send -t mysession:1.7 "sleep 2.2; rosservice call /inverse_kinematics_from_file/start" C-m
+tmux send -t mysession:1.7 "sleep 2.2; rosservice call /ik/inverse_kinematics_from_file/start" C-m
 #tmux send -t mysession:1.7 "ls -la" C-m
 #tmux setw synchronize-panes on
 
